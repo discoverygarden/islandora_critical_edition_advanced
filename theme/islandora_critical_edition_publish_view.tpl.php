93e951cb1a9968ebd5138c853114073d1fdce834
MorganDawe <mdawe@discoverygarden.com>
2013-11-20 16:43:57 +0000

    Added simple publish viewer for a critical aparatus datastream.

---
+++ theme/islandora_critical_edition_publish_view.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file
+ * This is the template file for the Critical Edition published object
+ *
+ */
+?>
+
+<div id="critical_edition_publish_form_wrapper">
+  <?php print $publish_form; ?>
+</div>
