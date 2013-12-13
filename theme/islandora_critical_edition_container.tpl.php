7ed54b7270cc7a7d0fa6ee7c76a261116c9a79c3
Alan Stanley <astanley@upei.ca>
2013-12-13 13:11:24 -0400

    Moved display logic

--- theme/islandora_critical_edition_container.tpl.php
+++ theme/islandora_critical_edition_container.tpl.php
@@ -5,10 +5,11 @@
  */
 // @todo Render out the critical apparatus object view.
 ?>
-<?php print drupal_render($apparatus_link); ?>
+<?php  print $edit_link; ?>
+
 <?php print drupal_render($apparatus); ?>
 <div class="edition-container">
  
-  <?php print drupal_render($edition_actions); ?>
+
 </div>
 
