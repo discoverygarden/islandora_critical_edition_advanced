9da9caa7a80ad8162439e56ede4d737b0fe4bbb1
Alan Stanley <astanley@upei.ca>
2013-12-17 14:48:31 -0400

    added divs to edit view

--- theme/islandora_critical_edition_container_edit.tpl.php
+++ theme/islandora_critical_edition_container_edit.tpl.php
@@ -5,12 +5,19 @@
  */
 // @todo Render out the critical apparatus object view.
 ?>
-<?php print "$view_link"; ?>
-<?php print drupal_render($apparatus_link); ?>
-<?php print drupal_render($toggle_workbench_membership_form); ?>
-<div class="edition-container">
-  <?php print drupal_render($version_actions); ?>
+<div id="workbench_toolbar">
+  <?php print "$view_link"; ?>
+  <?php print drupal_render($apparatus_link); ?>
+  <?php print drupal_render($toggle_workbench_membership_form); ?>
 </div>
-<?php print drupal_render($versions); ?>
-<?php print drupal_render($collation_actions); ?>
-<?php print drupal_render($collations); ?>
+
+<div class="edition-container">
+  <div class ="version_container">
+    <?php print drupal_render($version_actions); ?>
+    <?php print drupal_render($versions); ?>
+  </div>
+  <div class ="collation_container">
+    <?php print drupal_render($collation_actions); ?>
+    
+  </div>
+</div>
